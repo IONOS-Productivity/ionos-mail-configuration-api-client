a82d1a0dcb802ce2ed86761286a8d6600fd2ab7c
Misha M.-Kupriyanov <kupriyanov@strato.de>
2025-10-17 15:20:27 +0200

    chore: update update_templates.sh for shallow cloning of OpenAPI Generator
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

--- openapi-generator/update_templates.sh
+++ openapi-generator/update_templates.sh
@@ -5,11 +5,9 @@ tag_to_clone="v7.9.0"
 script_dir=$(realpath "$0" | xargs dirname)
 tmp_dir=$(mktemp -d)
 
-echo "Clone OpenAPI Generator ${tag_to_clone} to ${tmp_dir}"
-git clone https://github.com/OpenAPITools/openapi-generator.git "${tmp_dir}" || exit 1
+echo "Clone OpenAPI Generator ${tag_to_clone} to ${tmp_dir} (shallow clone)"
+git clone --depth 1 --branch "${tag_to_clone}" https://github.com/OpenAPITools/openapi-generator.git "${tmp_dir}" || exit 1
 cd "${tmp_dir}" || exit 1
-git checkout tags/${tag_to_clone}
-
 
 echo "Copy PHP templates"
 mkdir -p "${script_dir}/templates/php"
